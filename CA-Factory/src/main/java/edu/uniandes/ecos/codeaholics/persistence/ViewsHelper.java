01e3623cd8fbdac61d846fb5bd130718d0f8b26b
DavidMtz <df.martinez1@uniandes.edu.co>
2016-07-24 17:30:13 -0500

    [add] add viewhelper

---
+++ CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/persistence/ViewsHelper.java
@@ -0,0 +1,16 @@
+package edu.uniandes.ecos.codeaholics.persistence;
+
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+import java.util.HashMap;
+
+/**
+ * Created by davidMtz22 on 18/07/2016.
+ */
+public class ViewsHelper {
+
+	public static String render(String templatePath, HashMap<String, Object> model) {
+
+        return new FreeMarkerEngine().render(new ModelAndView(model, templatePath));
+    }
+}
