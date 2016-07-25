f999697e02e4c2702c9d0115051d7d44767482ab
snaphuman <fhernandezn@gmail.com>
2016-07-24 19:09:56 -0500

    [fix] enable cors to allow origins

--- CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/main/App.java
+++ CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/main/App.java
@@ -1,52 +1,76 @@
 package edu.uniandes.ecos.codeaholics.main;
 
-import static spark.Spark.before;
-import static spark.Spark.post;
-import static spark.Spark.get;
-import static spark.Spark.staticFiles;
-
 import edu.uniandes.ecos.codeaholics.business.CitizenServices;
 import edu.uniandes.ecos.codeaholics.config.Authorization;
 import edu.uniandes.ecos.codeaholics.config.DatabaseSingleton;
 import edu.uniandes.ecos.codeaholics.config.GeneralUtil;
+
+import static spark.Spark.*;
 
 /**
  * Created by snaphuman on 6/6/16.
  */
 public class App {
 
-	// Metodos
-	/***
-	 * Metodo principal del sistema.
-	 * 
-	 * @param args argunmentos
-	 */
-	public static void main(String[] args) {
+    // Metodos
 
-		// Initialize Database Connection
-		DatabaseSingleton.getInstance();
-		staticFiles.location("/public");
+    /***
+     * Metodo principal del sistema.
+     *
+     * @param args argunmentos
+     */
+    public static void main(String[] args) {
+
+        // Initialize Database Connection
+        DatabaseSingleton.getInstance();
+        staticFiles.location("/public");
         /*HTTPS line --- JLRM*/
         //secure("deploy/keystore.jks", "codeaholics", null, null);
-		
-		//Pruebas
-		get("/login", CitizenServices::login);
-		post("/login", CitizenServices::doLogin);
-		get("/signup", CitizenServices::signup);
-		post("/signup", CitizenServices::createUser);
 
-		//Rutas Ciudadano
-		post("/citizen/create", CitizenServices::insertCitizen, GeneralUtil.json());
-		post("/citizen/login", CitizenServices::doLogin, GeneralUtil.json());
-		
-		//Rutas Alcaldia
-		
-		//Admin Alcaldia
-		
-		//Rutas Mintic
-		
-		
-		before("/api/doc/*", Authorization::doDocAuthorization);
+        //Pruebas
+        get("/login", CitizenServices::login);
+        post("/login", CitizenServices::doLogin);
+        get("/signup", CitizenServices::signup);
+        post("/signup", CitizenServices::createUser);
 
-	}
+        //Rutas Ciudadano
+        post("/citizen/create", CitizenServices::insertCitizen, GeneralUtil.json());
+        post("/citizen/login", CitizenServices::doLogin, GeneralUtil.json());
+
+        //Rutas Alcaldia
+
+        //Admin Alcaldia
+
+        //Rutas Mintic
+
+
+        before("/api/doc/*", Authorization::doDocAuthorization);
+
+        /**
+         * Enable CORS in Spark Java to allow origins *
+         * @see https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+         */
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+    }
 }
