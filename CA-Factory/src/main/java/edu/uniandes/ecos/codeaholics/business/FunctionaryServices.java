6c0c0bf6447473a7ef6238e869bf41ba019e9566
sebascardonac11 <sebascardonac11@gmail.com>
2016-09-24 17:13:30 -0500

    [Procedures] Creando dummy de tramites pendientes

--- CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/business/FunctionaryServices.java
+++ CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/business/FunctionaryServices.java
@@ -4,12 +4,17 @@
 
 package edu.uniandes.ecos.codeaholics.business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bson.Document;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import edu.uniandes.ecos.codeaholics.config.DataBaseUtil;
 import spark.Request;
 import spark.Response;
 
@@ -18,6 +23,8 @@ public class FunctionaryServices {
 	private final static Logger log = LogManager.getLogger(AuthServices.class);
 	
 	private static Gson GSON = new GsonBuilder().serializeNulls().create();
+	
+	private static String PROCEDURES = "procedures";
 
 	/***
 	 * Consulta tramites asignados a un funcionario.
@@ -28,9 +35,37 @@ public class FunctionaryServices {
 	 *            response
 	 * @return mensaje de proceso exitoso
 	 */
-	public static String consultProcedures(Request req, Response res) {
-		log.info("Sebas");
-		return "success";
+	public static Object consultProcedures(Request pRequest, Response pResponse) {
+		List<Document> dataset = new ArrayList<>();
+		ArrayList<Document> documents = DataBaseUtil.getAll(PROCEDURES);
+		for (Document item : documents) {
+			item.remove("dependencies");
+			item.remove("procedures");
+			item.remove("address");
+			item.remove("url");
+			item.remove("phone");
+			item.remove("state");
+			item.remove("schedule");
+			dataset.add(item);
+		}
+		//ayuda para probar el servicio
+		if(documents.isEmpty()){
+			Document procedure = new Document();
+			procedure.put("date", "Girardot");
+			procedure.put("time", "Girardot");
+			procedure.put("fileNumber", "Girardot");
+			procedure.put("id", "Girardot");
+			procedure.put("name", "Girardot");
+			procedure.put("state", "Girardot");
+			procedure.put("attached", "Girardot");
+			
+			dataset.add(procedure);
+	
+		}
+
+		pResponse.type("application/json");
+		return dataset;
+
 	}
 
 	/***
