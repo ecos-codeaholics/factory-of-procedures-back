9d0006da36a9f85dff78def248375e709ca19b59
andres0sorio <osorio.af@gmail.com>
2016-11-10 14:10:45 -0500

    [NotifierSvc] Improved email notification service
    - added an EmailBuilder class to deal with email construction
    - template for each email resides under src/main/resources/email (4 templates were created: for registration, initiation of procedure, reset password and update of password).
    - please follow the format when configuring personalized emails (note the use of ##### pattern)
    - Simplified use of email sending method
    - UTF-8 compliance: this is one of the first services to be affected by the use of UTF-8 character
    - Added the corresponding unitary tests for each type of email
    - Tested on both windows/linux machines

--- CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/config/EmailNotifierSvc.java
+++ CA-Factory/src/main/java/edu/uniandes/ecos/codeaholics/config/EmailNotifierSvc.java
@@ -5,6 +5,8 @@
 package edu.uniandes.ecos.codeaholics.config;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -38,29 +40,19 @@ public class EmailNotifierSvc implements INotifierSvc {
 	private final static Logger log = LogManager.getLogger(EmailNotifierSvc.class);
 
 	public enum EmailType {
-
 		REGISTRATION, RESET, UPDATE, CHANGE, INITPROCEDURE;
-
 	}
+	
+	private static final String REGISTRATION_FILE = "src/main/resources/email/registration.properties";
+	private static final String RESET_FILE = "src/main/resources/email/reset.properties";
+	private static final String UPDATE_FILE = "src/main/resources/email/update.properties";
+	private static final String INITIATE_FILE = "src/main/resources/email/initiate.properties";
 
-	public class Email {
-
-		String body;
-		String subject;
-		String signature;
-
-	}
-
+	private static Map<EmailType,String> context = new HashMap<EmailType,String>();
+	
 	private static Properties mailServerProperties;
 	private static Session getMailSession;
 	private static MimeMessage generateMailMessage;
-
-	// private static String EMAIL_REGISTRATION_CONF =
-	// "src/main/resources/email/registration.properties";
-	// private static String EMAIL_RECOVERY_CONF =
-	// "src/main/resources/email/recovery.properties";
-	// private static String EMAIL_UPDATE_CONF =
-	// "src/main/resources/email/update.properties";
 
 	public EmailNotifierSvc() {
 
@@ -71,14 +63,12 @@ public class EmailNotifierSvc implements INotifierSvc {
 		mailServerProperties.put("mail.smtp.starttls.enable", "true");
 		log.debug("Mail Server Properties have been setup successfully..");
 		getMailSession = Session.getDefaultInstance(mailServerProperties, null);
-	}
-
-	public void add() {
-
-	}
-
-	public void build() {
-
+	
+		context.put(EmailType.REGISTRATION, REGISTRATION_FILE);
+		context.put(EmailType.RESET, RESET_FILE);
+		context.put(EmailType.UPDATE, UPDATE_FILE);
+		context.put(EmailType.INITPROCEDURE, INITIATE_FILE);
+		
 	}
 
 	/**
@@ -94,11 +84,11 @@ public class EmailNotifierSvc implements INotifierSvc {
 	 */
 	public void send(EmailType pContext, String pToEmail) throws AddressException, MessagingException {
 
-		if (pContext == EmailType.REGISTRATION) {
+		if (pContext.equals(EmailType.REGISTRATION)) {
 			sendRegister(pToEmail);
 		}
 	}
-	
+
 	/**
 	 * 
 	 * 
@@ -110,15 +100,10 @@ public class EmailNotifierSvc implements INotifierSvc {
 	 * @throws AddressException
 	 * @throws MessagingException
 	 */
-	public void send(EmailType pContext, String pToEmail, ArrayList<String> pParams) throws AddressException, MessagingException {
-
-		if (pContext == EmailType.RESET) {
-			sendReset(pToEmail, pParams);
-		} else if (pContext == EmailType.CHANGE) {
-			sendChange(pToEmail, pParams);
-		}else if(pContext == EmailType.INITPROCEDURE){
-			sendStartProcedure(pToEmail, pParams);
-		}
+	public void send(EmailType pContext, String pToEmail, ArrayList<String> pParams)
+			throws AddressException, MessagingException {
+	
+		sendWithParams(pContext, pToEmail, pParams);		
 	}
 
 	/**
@@ -130,47 +115,36 @@ public class EmailNotifierSvc implements INotifierSvc {
 	 * @throws MessagingException
 	 */
 	private void sendRegister(String pToEmail) throws AddressException, MessagingException {
-		String emailBody = "Su registro se ha realizado de manera exitosa en nuestro sistema. "
-				+ "<br><br> Cordial saludo, <br>Grupo Codeaholics";
-		sendEmail(pToEmail,"Bienvenido a la Fabrica de Tramites", emailBody);
-	}
-	
-	private void sendReset(String pToEmail, ArrayList<String> pParams) throws AddressException, MessagingException {
-		String emailBody = "Su clave ha sido restaurada de manera exitosa en nuestro sistema. "
-				+ "<br><br><br>Su nuevo password es: " + pParams.get(0)
-				+ "<br><br> Cordial saludo, <br>Grupo Codeaholics";
-		sendEmail(pToEmail,"Reseteo clave en el Sitema", emailBody);
+		
+		EmailBuilder email = new EmailBuilder(REGISTRATION_FILE);			
+		sendEmail(pToEmail, email.getSubject(), email.build());
 	}
 
-	private void sendChange(String pToEmail, ArrayList<String> pParams) throws AddressException, MessagingException {
-		String emailBody = "Su clave ha sido cambiada exitosamente en nuestro sistema. "
-				+ "<br><br><br>Su nueva clave es: " + pParams.get(0)
-				+ "<br><br> Cordial saludo, <br>Grupo Codeaholics";
-		sendEmail(pToEmail,"Cambio clave en el Sitema", emailBody);
+	private void sendWithParams(EmailType pContext, String pToEmail, ArrayList<String> pParams) throws AddressException, MessagingException {
+		
+		EmailBuilder email = new EmailBuilder(context.get(pContext));			
+		sendEmail(pToEmail, email.getSubject(), email.build(pParams));
 	}
-	
-	private void sendStartProcedure(String pToEmail, ArrayList<String> pParams) throws AddressException, MessagingException {
-		String emailBody = "Su solicitud de tramite fue registrada por nuestro sistema de forma exitosa. "
-				+ "<br><br><br>Solicitud: " + pParams.get(0)
-				+ "<br><br><br>Numero de Radicado: " + pParams.get(1)
-				+ "<br><br> Cordial saludo, <br>Grupo Codeaholics";
-		sendEmail(pToEmail,"Tramite Iniciado", emailBody);
-	}
-	
-	private void sendEmail(String pToEmail, String pSubject,String pEmailBody) throws AddressException, MessagingException {
+
+	private void sendEmail(String pToEmail, String pSubject, String pEmailBody)
+			throws AddressException, MessagingException {
+		
 		generateMailMessage = new MimeMessage(getMailSession);
 		generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(pToEmail));
 		generateMailMessage.setSubject(pSubject);
+		
 		String emailBody = pEmailBody;
 		generateMailMessage.setContent(emailBody, "text/html");
+		
 		log.debug("Mail Session has been created successfully..");
 		log.info("-----------------------------------");
 		log.info("Get Session and Send mail to recover the password");
 		log.info("-----------------------------------");
+		
 		Transport transport = getMailSession.getTransport("smtp");
 		transport.connect("smtp.gmail.com", "codeaholicsfactory@gmail.com", "codeaholics1");
 		transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-		transport.close();
+		transport.close();	
 	}
 
 }
